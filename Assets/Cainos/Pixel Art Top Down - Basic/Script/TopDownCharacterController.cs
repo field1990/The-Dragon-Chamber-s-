9a6d4a68c5fff1172c03e3e1c144bcf601b00fda
Owen Jones <71284174+OwenJRJones@users.noreply.github.com>
2022-03-24 12:15:16 -0300

    NPC Interaction
    Character can interact with golems now

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -10,14 +10,34 @@ namespace Cainos.PixelArtTopDown_Basic
 
         private Animator animator;
 
+        float horizontal;
+        float vertical;
+        Rigidbody2D rigidbody2d;
+
+        Vector2 lookDirection = new Vector2(1, 0);
+
         private void Start()
         {
             animator = GetComponent<Animator>();
+
+            rigidbody2d = GetComponent<Rigidbody2D>();
         }
 
 
         private void Update()
         {
+
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+
+            Vector2 move = new Vector2(horizontal, vertical);
+
+            if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+            {
+                lookDirection.Set(move.x, move.y);
+                lookDirection.Normalize();
+            }
+
             Vector2 dir = Vector2.zero;
             if (Input.GetKey(KeyCode.A))
             {
@@ -45,6 +65,21 @@ namespace Cainos.PixelArtTopDown_Basic
             animator.SetBool("IsMoving", dir.magnitude > 0);
 
             GetComponent<Rigidbody2D>().velocity = speed * dir;
+
+            //Code to detect raycast hit and trigger dialog box
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+                if (hit.collider != null)
+                {
+                    enemymover character = hit.collider.GetComponent<enemymover>();
+                    if (character != null)
+                    {
+                        character.DisplayDialog();
+                    }
+                }
+            }
         }
     }
 }
+
